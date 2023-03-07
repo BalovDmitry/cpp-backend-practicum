1a0b52a6eebfe81cdcea6367c18f816f106f980c
Balov Dmitry <balov.dimitri@yandex.ru>
2023-03-07 10:28:07 +0300

    Added final task.

---
+++ sprint1/problems/final_task/solution/src/main.cpp
@@ -0,0 +1,91 @@
+#include "sdk.h"
+//
+#include <boost/asio/io_context.hpp>
+#include <iostream>
+#include <thread>
+#include <boost/asio/signal_set.hpp>
+
+#include "json_loader.h"
+#include "request_handler.h"
+
+using namespace std::literals;
+namespace net = boost::asio;
+namespace sys = http_server::sys;
+
+namespace {
+
+// Запускает функцию fn на n потоках, включая текущий
+template <typename Fn>
+void RunWorkers(unsigned n, const Fn& fn) {
+    n = std::max(1u, n);
+
+    #ifdef __clang__
+        std::vector<std::thread> workers;
+    #else
+        std::vector<std::jthread> workers;
+    #endif
+
+    workers.reserve(n - 1);
+    // Запускаем n-1 рабочих потоков, выполняющих функцию fn
+    while (--n) {
+        workers.emplace_back(fn);
+    }
+    fn();
+
+    #ifdef __clang__
+        for (auto& worker : workers) {
+            worker.join();
+        }
+    #endif
+}
+
+}  // namespace
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: game_server <game-config-json>"sv << std::endl;
+        return EXIT_FAILURE;
+    }
+    try {
+        // 1. Загружаем карту из файла и построить модель игры
+        //std::string path = "/Users/balovdmitry/Desktop/SB/cpp-backend-practicum/sprint1/problems/map_json/precode/data/config.json";
+        //model::Game game = json_loader::LoadGame(path);
+        model::Game game = json_loader::LoadGame(argv[1]);
+
+        // 2. Инициализируем io_context
+        const unsigned num_threads = std::thread::hardware_concurrency();
+        net::io_context ioc(num_threads);
+
+        // 3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
+            // Подписываемся на сигналы и при их получении завершаем работу сервера
+        net::signal_set signals(ioc, SIGINT, SIGTERM);
+        signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
+            if (!ec) {
+                std::cout << "Signal "sv << signal_number << " received"sv << std::endl;
+                ioc.stop();
+            }
+        });
+
+        // 4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
+        http_handler::RequestHandler handler{game};
+
+        // 5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
+        const auto address = net::ip::make_address("127.0.0.1");
+        constexpr net::ip::port_type port = 8080;
+        http_server::ServeHttp(ioc, {address, port}, [&handler](auto&& req, auto&& send) {
+            handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+        });
+        
+
+        // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
+        std::cout << "Server has started..."sv << std::endl;
+
+        // 6. Запускаем обработку асинхронных операций
+        RunWorkers(std::max(1u, num_threads), [&ioc] {
+            ioc.run();
+        });
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+}
