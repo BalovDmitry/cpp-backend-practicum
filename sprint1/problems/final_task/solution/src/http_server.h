03fb1d6d52f7242d4d60641315e26f611dacff24
Balov Dmitry <balov.dimitri@yandex.ru>
2023-03-08 12:55:48 +0300

    Fixed request handler remarks.

--- sprint1/problems/final_task/solution/src/http_server.h
+++ sprint1/problems/final_task/solution/src/http_server.h
@@ -97,19 +97,19 @@ public:
         // Обработчики асинхронных операций acceptor_ будут вызываться в своём strand
         , acceptor_(net::make_strand(ioc))
         , request_handler_(std::forward<Handler>(request_handler)) {
-        // Открываем acceptor, используя протокол (IPv4 или IPv6), указанный в endpoint
-        acceptor_.open(endpoint.protocol());
+            // Открываем acceptor, используя протокол (IPv4 или IPv6), указанный в endpoint
+            acceptor_.open(endpoint.protocol());
 
-        // После закрытия TCP-соединения сокет некоторое время может считаться занятым,
-        // чтобы компьютеры могли обменяться завершающими пакетами данных.
-        // Однако это может помешать повторно открыть сокет в полузакрытом состоянии.
-        // Флаг reuse_address разрешает открыть сокет, когда он "наполовину закрыт"
-        acceptor_.set_option(net::socket_base::reuse_address(true));
-        // Привязываем acceptor к адресу и порту endpoint
-        acceptor_.bind(endpoint);
-        // Переводим acceptor в состояние, в котором он способен принимать новые соединения
-        // Благодаря этому новые подключения будут помещаться в очередь ожидающих соединений
-        acceptor_.listen(net::socket_base::max_listen_connections);
+            // После закрытия TCP-соединения сокет некоторое время может считаться занятым,
+            // чтобы компьютеры могли обменяться завершающими пакетами данных.
+            // Однако это может помешать повторно открыть сокет в полузакрытом состоянии.
+            // Флаг reuse_address разрешает открыть сокет, когда он "наполовину закрыт"
+            acceptor_.set_option(net::socket_base::reuse_address(true));
+            // Привязываем acceptor к адресу и порту endpoint
+            acceptor_.bind(endpoint);
+            // Переводим acceptor в состояние, в котором он способен принимать новые соединения
+            // Благодаря этому новые подключения будут помещаться в очередь ожидающих соединений
+            acceptor_.listen(net::socket_base::max_listen_connections);
     }
 
     void Run() {
