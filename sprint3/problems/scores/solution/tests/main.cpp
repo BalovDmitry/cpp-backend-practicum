126634db14ba6d78d587f3c9524d5a13c4442830
Balov Dmitry <balov.dimitri@yandex.ru>
2023-06-26 11:12:06 +0300

    Removed final solution.

--- sprint3/problems/scores/solution/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_session.hpp>
-#include <iostream>
-
-#include "../src/extra_data.h"
-
-int main(int argc, char* argv[]) {
-    int result = Catch::Session().run(argc, argv);
-    return result;
-} 
